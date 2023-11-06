25897ca1494a2ca3acd487f476a8781a48334af4
Tamim <s163.tamim@gmail.com>
2023-11-07 00:39:55 +0600

    setup

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000;
+// env
+require('dotenv').config()
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.vjc6ohr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('The pipes server is running all the time..........')
+})
+
+app.listen(port, () => {
+  console.log(`The pipes app listening on port ${port}`)
+})
